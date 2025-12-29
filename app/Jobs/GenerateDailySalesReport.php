1183ea236b36e560362f385b6d3bedd3cb21f9e2
Jessie Siat <jcris.siat@gmail.com>
2025-12-29 15:55:44 +0800

    update collection mapping

--- app/Jobs/GenerateDailySalesReport.php
+++ app/Jobs/GenerateDailySalesReport.php
@@ -28,13 +28,23 @@ class GenerateDailySalesReport implements ShouldQueue
         $today = now()->toDateString();
 
         $cartItemsToday = CartItem::whereHas('cart', function ($query) use ($today) {
-            $query->whereDate('payment_date', $today);
-        })
+                $query->whereDate('payment_date', $today);
+            })
             ->with('product')
             ->get();
-
+    
         // Group cart items by product id
-        $groupedCartItems = $cartItemsToday->groupBy('product_id');
+        $groupedCartItems = $cartItemsToday
+            ->groupBy('product_id')
+            ->map(function ($items) {
+                $productName = optional($items->first()->product)->name;
+                $quantitySum = $items->sum('quantity');
+
+                return [
+                    'product_name' => $productName,
+                    'total_quantity' => $quantitySum,
+                ];
+            });
 
         Mail::to('admin@example.com')->queue(new DailySalesReport($groupedCartItems));
     }
